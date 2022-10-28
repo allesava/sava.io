7d4e52a2f6169f61054161bc9cab102d49fc1f4b
Alina Sava <alina@sava.io>
2022-10-28 11:54:52 +0200

    [2022.10.28] initial deploy

---
+++ assets/js/nav.js
@@ -0,0 +1,139 @@
+/*! Version 22.10.28a */
+// let projects = document.getElementById('projects');
+// let closeProjects = document.getElementById('closeProjects');
+
+let about = document.getElementById('about');
+let linkAbout = document.getElementById('linkAbout');
+let closeAbout = document.getElementById('closeAbout');
+
+let work = document.getElementById('work');
+let linkWork = document.getElementById('linkWork');
+let closeWork = document.getElementById('closeWork');
+
+let imprint = document.getElementById('imprint');
+let linkImprint = document.getElementById('linkImprint');
+let closeImprint = document.getElementById('closeImprint');
+
+let privacy = document.getElementById('privacy');
+let linkPrivacy = document.getElementById('linkPrivacy');
+let closePrivacy = document.getElementById('closePrivacy');
+
+closeIntro.onclick = function () {
+    toggleIntro();
+}
+
+function toggleIntro()  {
+    let svgClose = document.getElementById('intro-svg');
+
+    if (introContent.style.display === 'none') {
+        introContent.style.display = 'block';
+        svgClose.style.transform = 'none';
+    } else {
+        introContent.style.display = 'none';
+        svgClose.style.transform = 'rotate(45deg)';
+    }
+}
+
+linkAbout.onclick = function () {
+    displayAbout();
+}
+
+linkWork.onclick = function () {
+    displayWork();
+}
+
+linkImprint.onclick = function () {
+    displayImprint();
+    linkImprint.classList.toggle('active');
+}
+
+linkPrivacy.onclick = function () {
+    displayPrivacy();
+    linkPrivacy.classList.toggle('active');
+}
+
+closeAbout.onclick = function () {
+    hideAbout();
+}
+
+closeWork.onclick = function () {
+    hideWork();
+}
+
+closeImprint.onclick = function () {
+    hideImprint();
+    linkImprint.classList.remove('active');
+}
+
+closePrivacy.onclick = function () {
+    hidePrivacy();
+    linkPrivacy.classList.remove('active');
+}
+
+function displayAbout() {
+    about.classList.toggle('inUp');
+    about.classList.toggle('outUp');
+
+    work.classList.remove('inUp');
+    work.classList.add('outUp');
+    imprint.classList.remove('inUp');
+    imprint.classList.add('outUp');
+    privacy.classList.remove('inUp');
+    privacy.classList.add('outUp');
+}
+
+function displayWork() {
+    work.classList.toggle('inUp');
+    work.classList.toggle('outUp');
+
+    about.classList.remove('inUp');
+    about.classList.add('outUp');
+    imprint.classList.remove('inUp');
+    imprint.classList.add('outUp');
+    privacy.classList.remove('inUp');
+    privacy.classList.add('outUp');
+}
+
+function displayImprint() {
+    imprint.classList.toggle('inUp');
+    imprint.classList.toggle('outUp');
+
+    about.classList.remove('inUp');
+    about.classList.add('outUp');
+    work.classList.remove('inUp');
+    work.classList.add('outUp');
+    privacy.classList.remove('inUp');
+    privacy.classList.add('outUp');
+}
+
+function displayPrivacy() {
+    privacy.classList.toggle('inUp');
+    privacy.classList.toggle('outUp');
+
+    about.classList.remove('inUp');
+    about.classList.add('outUp');
+    work.classList.remove('inUp');
+    work.classList.add('outUp');
+    imprint.classList.remove('inUp');
+    imprint.classList.add('outUp');
+}
+
+function hideAbout()  {
+    about.classList.remove('inUp');
+    about.classList.add('outUp');
+}
+
+function hideWork()  {
+    work.classList.remove('inUp');
+    work.classList.add('outUp');
+}
+
+function hideImprint()  {
+    imprint.classList.remove('inUp');
+    imprint.classList.add('outUp');
+}
+
+function hidePrivacy()  {
+    privacy.classList.remove('inUp');
+    privacy.classList.add('outUp');
+}
